ac338de8a49b4156e03c859c3d572132c75e4a4f
lilcashpl <longwaytodreampl@gmail.com>
2023-11-28 22:44:53 +0100

    Streamline SDK 2.2.1 Release

---
+++ tools/gitVersion.h
@@ -0,0 +1,7 @@
+#ifndef _GIT_VERSION_HEADER 
+#define _GIT_VERSION_HEADER 
+// Auto generated - start 
+#define GIT_LAST_COMMIT "SHA: 7ac42e4" 
+#define GIT_LAST_COMMIT_SHORT "7ac42e4" 
+// Auto generated - end 
+#endif 
--- tools/packman/bootstrap/configure.bat
+++ tools/packman/bootstrap/configure.bat
@@ -1,3 +1,4 @@
+
 :: Copyright 2019 NVIDIA CORPORATION
 ::
 :: Licensed under the Apache License, Version 2.0 (the "License");
